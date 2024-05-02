dd9ee760e636c69c8034a144ce1f3337521c0efa
Muhammad Syahri Romadhoni <121951868+MsrXvi@users.noreply.github.com>
2024-05-02 22:29:13 +0700

    Next JS Food

---
+++ app/meals/error.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Error(){
+    return (
+        <main className= "error">
+            <h1>An error osscured</h1>
+            <p>Failed to fetch meals data, Please reload or Try again later</p>
+        </main>
+    )
+}
